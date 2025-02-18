b15f5be266fcaca961357920ae6e90633a8fb3f7
Bernard <oyebenard@gmail.com>
2025-02-18 16:43:16 -0600

    Add responsiveness to pages

--- src/components/JoinCommunity.js
+++ src/components/JoinCommunity.js
@@ -17,17 +17,17 @@ function JoinCommunity({ isOpen, onClose }) {
                 modal: {
                     background: "#FFFFFF33",
                     backdropFilter: "blur(5px)",
-                    border: "1px",
-                    borderColor: "#fff",
-                    width: "40%",
+                    border: "1px solid #FFFFFF66",
+                    width: "90%",
+                    maxWidth: "500px",
                     borderRadius: "4px",
-                    padding: "20px"
+                    padding: "20px",
                 }
             }}
         >
             <div>
                 <div className='w-full flex justify-center'>
-                    <img src={require("../assets/images/hourglass.png")} className='h-20' />
+                    <img src={require("../assets/images/hourglass.png")} className='h-20' alt='hour glass' />
                 </div>
                 <h2 className="text-xl font-bold py-4 text-white text-center">👋 JOIN THE COMMUNITY, BE THE FIRST TO KNOW!</h2>
                 <p className="text-sm text-white mb-8 text-center">
@@ -38,11 +38,11 @@ function JoinCommunity({ isOpen, onClose }) {
                 <div className="mt-4">
                     <p className='text-sm text-white mb-2'>Enter Your Email to Join the Waitlist</p>
                     <div className='relative w-full'>
-                        <MdOutlineMail className='text-white absolute left-3 top-1/2 transform -translate-y-1/2 text-lg' />
+                        <MdOutlineMail className='text-[#FFFFFF66] absolute left-3 top-1/2 transform -translate-y-1/2 text-lg' />
                         <input
                             type="email"
                             placeholder="Enter email address"
-                            className="bg-transparent border-b border-white py-2 text-sm text-white w-full pl-10 focus:outline-none"
+                            className="bg-transparent border-b border-[#FFFFFF66] py-2 text-sm text-white w-full pl-10 focus:outline-none"
                         />
                     </div>
                     <button className="mt-4 w-full text-center bg-transparent text-white py-2 rounded-md font-bold text-xl">
@@ -52,7 +52,7 @@ function JoinCommunity({ isOpen, onClose }) {
 
             </div>
         </Modal>
-    )
+    );
 }
 
-export default JoinCommunity
+export default JoinCommunity;
--- src/pages/LaunchingSoon.jsx
+++ src/pages/LaunchingSoon.jsx
@@ -14,13 +14,13 @@ export default function LaunchingSoon() {
                     <img src={require('../assets/images/harambee.png')} alt="harambee icon" />
                 </div>
 
-                <div className='text-center'>
-                    <h1 className='text-6xl font-semibold text-white drop-shadow-[0_2px_2px_rgb(0,105,116)] mb-8'>
+                <div className='text-center px-4 md:px-0'>
+                    <h1 className='text-4xl md:text-6xl font-semibold text-white drop-shadow-[0_2px_2px_rgb(0,105,116)] mb-8'>
                         LAUNCHING SOON !!!
                     </h1>
                     <button
                         onClick={() => setIsModalOpen(true)}
-                        className='bg-white py-4 px-32 rounded-full border border-[#38D3E3]'
+                        className='bg-white w-full md:w-[80%] h-16 rounded-full border border-[#38D3E3]'
                     >
                         <span className='bg-gradient-to-b from-[#38D3E3] to-[#006974] inline-block text-transparent bg-clip-text font-bold'>
                             JOIN THE WAITING LIST
@@ -28,7 +28,7 @@ export default function LaunchingSoon() {
                     </button>
                 </div>
 
-                <div className='flex h-80'>
+                <div className='flex md:h-80'>
                     <img src={require('../assets/images/hand.png')} alt="harambee icon" />
                 </div>
             </section>
