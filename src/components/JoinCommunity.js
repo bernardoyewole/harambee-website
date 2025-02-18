84a794166bf3fb6354b3eb02c04996fcaadf32a5
Bernard <oyebenard@gmail.com>
2025-02-18 15:10:55 -0600

    Add subscribe modal

---
+++ src/components/JoinCommunity.js
@@ -0,0 +1,58 @@
+import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { MdOutlineMail } from "react-icons/md";
+
+function JoinCommunity({ isOpen, onClose }) {
+    return (
+        <Modal
+            open={isOpen}
+            onClose={onClose}
+            closeIcon={<IoIosCloseCircleOutline color='white' size={26} />}
+            center
+            styles={{
+                overlay: {
+                    background: "rgba(0, 0, 0, 0.8)",
+                },
+                modal: {
+                    background: "#FFFFFF33",
+                    backdropFilter: "blur(5px)",
+                    border: "1px",
+                    borderColor: "#fff",
+                    width: "40%",
+                    borderRadius: "4px",
+                    padding: "20px"
+                }
+            }}
+        >
+            <div>
+                <div className='w-full flex justify-center'>
+                    <img src={require("../assets/images/hourglass.png")} className='h-20' />
+                </div>
+                <h2 className="text-xl font-bold py-4 text-white text-center">👋 JOIN THE COMMUNITY, BE THE FIRST TO KNOW!</h2>
+                <p className="text-sm text-white mb-8 text-center">
+                    We're building something amazing! Be among the first to experience personalized care services and support.
+                    Sign up to secure your spot and get exclusive updates.
+                </p>
+
+                <div className="mt-4">
+                    <p className='text-sm text-white mb-2'>Enter Your Email to Join the Waitlist</p>
+                    <div className='relative w-full'>
+                        <MdOutlineMail className='text-white absolute left-3 top-1/2 transform -translate-y-1/2 text-lg' />
+                        <input
+                            type="email"
+                            placeholder="Enter email address"
+                            className="bg-transparent border-b border-white py-2 text-sm text-white w-full pl-10 focus:outline-none"
+                        />
+                    </div>
+                    <button className="mt-4 w-full text-center bg-transparent text-white py-2 rounded-md font-bold text-xl">
+                        SUBMIT →
+                    </button>
+                </div>
+
+            </div>
+        </Modal>
+    )
+}
+
+export default JoinCommunity
--- src/pages/LaunchingSoon.jsx
+++ src/pages/LaunchingSoon.jsx
@@ -1,21 +1,39 @@
+import { useState } from 'react';
+import JoinCommunity from '../components/JoinCommunity';
+
 export default function LaunchingSoon() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <section style={{ backgroundImage: `url(${require('../assets/images/bg-index-gradient.png')})` }}
-            className='bg-cover h-full w-full flex flex-col items-center justify-between'>
-            <div className='flex justify-center h-48'>
-                <img src={require('../assets/images/harambee.png')} alt="harambee icon" />
-            </div>
-            <div className='text-center'>
-                <h1 className='text-6xl font-semibold text-white drop-shadow-[0_2px_2px_rgb(0,105,116)] mb-8'>LAUNCHING SOON !!!</h1>
-                <button className='bg-white py-4 px-32 rounded-full border-4'>
-                    <span className='bg-gradient-to-b from-[#38D3E3] to-[#006974] inline-block text-transparent bg-clip-text font-bold'>
-                        JOIN THE WAITING LIST
-                    </span>
-                </button>
-            </div>
-            <div className='flex h-80'>
-                <img src={require('../assets/images/hand.png')} alt="harambee icon" />
-            </div>
-        </section>
-    )
+        <>
+            <section
+                style={{ backgroundImage: `url(${require('../assets/images/bg-index-gradient.png')})` }}
+                className='bg-cover h-full w-full flex flex-col items-center justify-between'
+            >
+                <div className='flex justify-center h-48'>
+                    <img src={require('../assets/images/harambee.png')} alt="harambee icon" />
+                </div>
+
+                <div className='text-center'>
+                    <h1 className='text-6xl font-semibold text-white drop-shadow-[0_2px_2px_rgb(0,105,116)] mb-8'>
+                        LAUNCHING SOON !!!
+                    </h1>
+                    <button
+                        onClick={() => setIsModalOpen(true)}
+                        className='bg-white py-4 px-32 rounded-full border border-[#38D3E3]'
+                    >
+                        <span className='bg-gradient-to-b from-[#38D3E3] to-[#006974] inline-block text-transparent bg-clip-text font-bold'>
+                            JOIN THE WAITING LIST
+                        </span>
+                    </button>
+                </div>
+
+                <div className='flex h-80'>
+                    <img src={require('../assets/images/hand.png')} alt="harambee icon" />
+                </div>
+            </section>
+
+            <JoinCommunity isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+        </>
+    );
 }
