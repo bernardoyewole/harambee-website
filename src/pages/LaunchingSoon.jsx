1e361fc5601314e2d163cbb743ba4950b6423e37
Bernard <oyebenard@gmail.com>
2025-02-19 22:56:44 -0600

    Add video toggle on mobile screens

--- src/pages/LaunchingSoon.jsx
+++ src/pages/LaunchingSoon.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
+import { motion, AnimatePresence } from "framer-motion";
 import JoinCommunity from '../components/JoinCommunity';
+import { HiOutlinePlay } from "react-icons/hi2";
+import { BiHide } from "react-icons/bi";
 
 export default function LaunchingSoon() {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [showVideo, setShowVideo] = useState(false);
 
     return (
         <>
@@ -32,17 +36,65 @@ export default function LaunchingSoon() {
                 <div className='flex md:h-80'>
                     <img src={require('../assets/images/hand.png')} alt="harambee icon" />
                 </div>
-                <div className="absolute bottom-6 right-6 w-[90%] md:w-[480px] aspect-video rounded-3xl overflow-hidden">
-                    <iframe
-                        src="https://www.youtube.com/embed/HvPHBf_tXxI?si=i9JvAjWuipkcWODn"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin"
-                        allowFullScreen
-                        className="w-full h-full"
-                    />
-                </div>
+
+                <AnimatePresence>
+                    {showVideo && (
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.9 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0.9 }}
+                            transition={{ duration: 0.4, ease: "easeInOut" }}
+                            className="absolute bottom-[74px] right-4 w-[90%] md:w-[480px] aspect-video rounded-3xl overflow-hidden"
+                        >
+                            <iframe
+                                src="https://www.youtube.com/embed/HvPHBf_tXxI?si=i9JvAjWuipkcWODn"
+                                title="YouTube video player"
+                                frameBorder="0"
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerPolicy="strict-origin-when-cross-origin"
+                                allowFullScreen
+                                className="w-full h-full"
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+
+                <motion.button
+                    whileTap={{ scale: 0.95 }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    onClick={() => setShowVideo(!showVideo)}
+                    className="absolute bottom-4 right-4 bg-white py-3 w-48 rounded-full flex items-center justify-center text-black border border-gray-300 shadow-md md:hidden"
+                >
+                    <motion.span className='flex items-center justify-center gap-2 w-full'>
+                        <AnimatePresence mode="popLayout">
+                            {showVideo ? (
+                                <motion.span
+                                    key="hide"
+                                    initial={{ opacity: 0, y: 5 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -5 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="flex items-center gap-2"
+                                >
+                                    <BiHide size={24} />
+                                    <span className='text-gray-800 text-sm ml-1'>Hide Intro Video</span>
+                                </motion.span>
+                            ) : (
+                                <motion.span
+                                    key="show"
+                                    initial={{ opacity: 0, y: 5 }}
+                                    animate={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0, y: -5 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="flex items-center gap-2"
+                                >
+                                    <HiOutlinePlay size={24} />
+                                    <span className='text-gray-800 text-sm'>Watch Intro Video</span>
+                                </motion.span>
+                            )}
+                        </AnimatePresence>
+                    </motion.span>
+                </motion.button>
             </section>
 
             <JoinCommunity isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
